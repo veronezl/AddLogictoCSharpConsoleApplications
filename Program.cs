ca07db2436427d500cd3c0fc8cf19e52f5456896
veronezl <lucasveronez.dev@gmail.com>
2025-01-06 15:58:24 -0300

    Operadores condicionais

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata;
 
 // Evaluate Boolean Expressions to Make Decisions in C# -----------------------------------------------------
 
@@ -67,3 +68,96 @@ string s1 = "Hello";
 string s2 = "Hello";
 Console.WriteLine(s1 != s2); // output: False
 */
+
+// Operador condicional ----------------------------------------------------------------------
+
+/*
+int saleAmount = 1001;
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}") -> Embutido
+int discount = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"Discount: {discount}");
+*/
+
+// Desafio de condicinal --------------------------------------------------------------------
+
+/*
+Random coin = new Random();
+int flip = coin.Next(0, 2); // ou direto: Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
+Console.WriteLine((flip == 0) ? "heads" : "tails");
+*/
+
+// Desafio de lógica de decisão --------------------------------------------------------------
+
+/*
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+*/
+
+// Controlar o escopo da variável e a lógica usando blocos de código em C# ***************************************
+
+/*
+bool flag = true;
+int value = 0;
+
+if (flag)
+{
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
+*/
+
+// Exemple de if sem {}
+/*
+string name = "steve";
+if (name == "bob") Console.WriteLine("Found Bob");
+else if (name == "steve") Console.WriteLine("Found Steve");
+else Console.WriteLine("Found Chuck");
+*/
+
+// Desafio de escopo de variável -------------------------------------------------------------
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;
+    
+}
+
+if (found) 
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
