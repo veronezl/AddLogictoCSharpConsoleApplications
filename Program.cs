386e97e01a939a2a85deb2c975c6ec0a31691c51
veronezl <lucasveronez.dev@gmail.com>
2025-01-09 12:23:47 -0300

    Estudo de for com if e foreach

--- Program.cs
+++ Program.cs
@@ -200,6 +200,8 @@ Console.WriteLine($"{employeeName}, {title}");
 // SKU value format: <product #>-<2-letter color code>-<size code>
 
 // SKU = Stock Keeping Unit
+
+/*
 string sku = "01-MN-L";
 
 string[] product = sku.Split('-');
@@ -254,3 +256,91 @@ switch (product[2])
 }
 
 Console.WriteLine($"Product: {size} {color} {type}");
+*/
+
+// Iterar por um bloco de código usando a instrução for em C# *************************************************
+
+
+/*
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if ( i == 7) break;
+}
+*/
+
+// Exemplo MUITO IMPORTANTE DE FOR --------------********************
+
+/*
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+*/
+
+/*
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
+*/
+
+// Mesmo código, porem, com chaves 
+
+/*
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+for (int i = 0; i < names.Length; i++)
+{
+    if (names[i] == "David")
+    {
+        names[i] = "Sammy";
+    }
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+*/
+
+// Desafio for e if  - FizzBuzz -------------------------------------------------------------------------
+
+/*
+for (int count = 1; count <= 100; count++) 
+{
+    if (count % 3 == 0) 
+    {
+        Console.WriteLine($"{count} Fizz");
+    }
+    else if (count % 5 == 0) 
+    {
+        Console.WriteLine($"{count} Buzz");
+    }
+    else if (count % 3 == 0 && count % 5 == 0) 
+    {
+        Console.WriteLine($"{count} FizzBuzz");
+    }
+    else {
+        Console.WriteLine(count);
+    }
+}
+*/
+
+// Outro modelo de resolver 
+
+/*
+for (int i = 1; i < 101; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else
+        Console.WriteLine($"{i}");
+}
+*/
